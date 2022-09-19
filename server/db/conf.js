757659a063608bb3d500c256212f84a1af44b3d9
missht0 <missht0@163.com>
2022-09-19 13:56:06 +0800

    创建项目

---
+++ server/db/conf.js
@@ -0,0 +1,22 @@
+let conf = {
+  host: 'localhost',
+  port: 3306,
+  database: 'hznu',
+  user:     'root',
+  password: 'rootroot',
+  multipleStatements: true,
+  secret: 'hznu2022',
+};
+
+module.exports = conf;
+// let conf = {
+//   host: '121.40.124.170',
+//   port: 3306,
+//   database: 'hznu',
+//   user:     'hznu',
+//   password: '123456',
+//   multipleStatements: true,
+//   secret: 'hznu2022',
+// };
+
+// module.exports = conf;
